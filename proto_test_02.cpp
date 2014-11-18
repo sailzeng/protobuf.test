b0b51ca44cd690a526e51b599593d3e09811633e
Sailzeng <fullsail@163.com>
2014-11-18 23:10:50 +0800

    继续测试。

--- proto_test_02.cpp
+++ proto_test_02.cpp
@@ -17,17 +17,36 @@ int main(int argc, const char *argv[])
 {
     google::protobuf::compiler::DiskSourceTree sourceTree;
     //look up .proto file in current directory
-    sourceTree.MapPath("", "./");
+    sourceTree.MapPath("", "proto");
+
+    //google::protobuf::compiler::MultiFileErrorCollector error_collector;
+
     google::protobuf::compiler::Importer importer(&sourceTree, NULL);
     //runtime compile foo.proto
-    importer.Import("foo.proto");
+    const google::protobuf::FileDescriptor *open_result = importer.Import("test_02.proto");
+    if (!open_result)
+    {
+        std::cout << "Open file fail. Please check your. code." << std::endl;
+        return -1;
+    }
 
-    const google::protobuf::Descriptor *descriptor = importer.pool()->FindMessageTypeByName("Pair");
-    std::cout << descriptor->DebugString();
+    //可以发现内嵌类，用.操作的方法
+    const google::protobuf::Descriptor *desc_s4 = importer.pool()->FindMessageTypeByName("N01.N02.S3.S4");
+    if (desc_s4)
+    { 
+        std::cout << desc_s4->DebugString();
+    }
+    
+    //测试一下如何使用反射。
+    const google::protobuf::Descriptor *desc_s3 = importer.pool()->FindMessageTypeByName("N01.N02.S3");
+    if (desc_s3)
+    {
+        std::cout << desc_s3->DebugString();
+    }
 
     // build a dynamic message by "Pair" proto
     google::protobuf::DynamicMessageFactory factory;
-    const google::protobuf::Message *message = factory.GetPrototype(descriptor);
+    const google::protobuf::Message *message = factory.GetPrototype(desc_s3);
     // create a real instance of "Pair"
     google::protobuf::Message *pair = message->New();
 
@@ -35,9 +54,9 @@ int main(int argc, const char *argv[])
     const google::protobuf::Reflection *reflection = pair->GetReflection();
 
     const google::protobuf::FieldDescriptor *field = NULL;
-    field = descriptor->FindFieldByName("key");
+    field = desc_s3->FindFieldByName("key");
     reflection->SetString(pair, field, "my key");
-    field = descriptor->FindFieldByName("value");
+    field = desc_s3->FindFieldByName("value");
     reflection->SetUInt32(pair, field, 1111);
 
     std::cout << pair->DebugString();
